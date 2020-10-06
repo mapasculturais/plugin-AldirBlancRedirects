c7de0334b2bbbb11be5172d96d8c1d5ae7b42fd9
Everaldo Matias <matiaseveraldo@gmail.com>
2020-10-06 17:23:42 -0300

    Remove redirecionamentos dos formularios do Inciso 3

--- Plugin.php
+++ Plugin.php
@@ -20,6 +20,16 @@ class Plugin extends \MapasCulturais\Plugin {
                 ['file', 'privateFile'],
             ];
 
+            // não redireciona quando acessa formulários do inciso 3
+            if ($this->id == 'opportunity') {
+                $requestedEntity = $this->requestedEntity;
+                $opportunitiesInciso3 = $app->controller('AldirBlanc')->getOpportunitiesInciso3();
+
+                if (in_array($requestedEntity, $opportunitiesInciso3)) {
+                    return;
+                }
+            }
+
             if (in_array([$this->id, $this->action], $enabled_routes)) {
                 return;
             }
